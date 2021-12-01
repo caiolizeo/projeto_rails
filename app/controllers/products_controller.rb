1b53456b360c68bd26b191017e13bf35835aa1d0
caiolizeo <caio-lz@hotmail.com>
2021-12-01 14:16:08 -0300

    adicionada função de cadastrar produto e pagina de checkout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,14 +4,43 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.create(name: params[:product][:name],
+                           description: params[:product][:description],
+                           category: params[:product][:category],
+                           value: params[:product][:value],
+                           selected: 0
+                          )
+    
+    redirect_to root_path
+    
+  end
+
   def update
     product = Product.find params[:id]
-    product.update(:selected => 1)
+
+    if product.selected.zero?
+      product.update(:selected => 1) 
+    else
+      product.update(:selected => 0)
+    end
+
     redirect_to root_path
   end
 
-  def destroy
-    Product.delete params[:id]
+  def checkout
+    @products = Product.where(selected: 1)
+  end
+
+  def finish
+    Product.where(selected: 1).each do |product|
+      product.delete
+    end
     redirect_to root_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :products, only: [:show, :update, :destroy]
+  resources :products, only: [:show, :new, :create, :update]
+
+  get '/checkout', to: 'products#checkout'
+  delete '/checkout/finish', to: 'products#finish'
+
 end
